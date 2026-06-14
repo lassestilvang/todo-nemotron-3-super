919b2f5ada92f360e85867e45b4e31a02a449b3d
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-06-14 23:10:12 +0200

    test: remove obsolete test files

--- test/use-debounce.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'bun:test';
-
-describe('useDebounce', () => {
-  it('hook can be imported', async () => {
-    const mod = await import('@/hooks/use-debounce');
-    expect(mod.default).toBeDefined();
-    expect(typeof mod.default).toBe('function');
-  });
-});
